19919cd19de7fdb83fef61fdf4cae1e5c57b62e4
ServerIbraimovUa <127730579+ServerIbraimovUa@users.noreply.github.com>
2023-05-12 23:10:15 +0300

    g

---
+++ src/js/slide-product.js
@@ -0,0 +1,15 @@
+$('.product-wrapper').slick({
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: false,
+  vertical: true,
+});
+
+$('.page-gallery__list').slick({
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: false,
+  autoplaySpeed: 3000,
+});
